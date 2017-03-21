b29e5e4e48a1561dfb68412c1fdad0b764c0c621
BKagadey <kagadey2@gmail.com>
2017-03-21 14:52:38 +0200

    Base stucture!

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@
  */
 var $ = require('jquery');
 var str = require('./constant');
+require('./less');
 
 var App = function() {
     // console.log(str);
-    $('body').html(str);
+    $('body').html('<div class="b-test"><h3 class="b-test__heading">Heading</h3></div>');
 };
 
 var app = new App();
---
+++ src/less/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by kagadey on 21.03.2017.
+ */
+require('./main.less');
