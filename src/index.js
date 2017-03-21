890caab7ad2d842f0c3987350c3ea6d362fdad6f
BKagadey <kagadey2@gmail.com>
2017-03-21 14:18:49 +0200

    Base stucture

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kagadey on 21.03.2017.
+ */
+var $ = require('jquery');
+var str = require('./constant');
+
+var App = function() {
+    // console.log(str);
+    $('body').html(str);
+};
+
+var app = new App();
+app.get = function() {};
